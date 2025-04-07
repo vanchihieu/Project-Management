851fd2bb4f507393b4177c81a0a7ba13084d8f45
Van Chi Hieu <vanchihieu3@gmail.com>
2025-04-07 12:12:43 +0700

    Implement sign-in functionality with OAuth support and create SignInCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
     <div className="bg-blue-500 text-white p-4 rounded-md">
       Tailwind đang hoạt động!
+      <Input/>
       <Button variant={"primary"}>Click vao</Button>
       <Button variant={"destructive"}>Click vao</Button>
       <Button variant={"ghost"}>Click vao</Button>
